8b92776d74a07a6388fe72beb98d352774b5edb9
Zhikeng <114634385+Zhikeng@users.noreply.github.com>
2023-05-20 11:38:43 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,22 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
-  if(!err) {
-    console.log("Successful connection with MongoDB Server");  
+// mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+//   if(!err) {
+//     console.log("Successful connection with MongoDB Server");  
+//   }
+//   else {
+//       console.log("Error with MongoDB's connectivity");
+//       console.log(err)
+//   }
+// });
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
   }
-  else {
-      console.log("Error with MongoDB's connectivity");
-      console.log(err)
-  }
-});
+}
