fecbdbc575946bd46f4ba046d38883b08a7a046f
Zhikeng <114634385+Zhikeng@users.noreply.github.com>
2023-05-20 11:57:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.all('*', (req,res) => {
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.DB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
